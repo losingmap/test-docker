29a9571c9069b272f271201c4aef07c36ff3ba76
LFT <fentloe@qq.com>
2024-07-14 19:04:00 +0800

    init

---
+++ src/main/java/com/lft/testdocker/TestDockerApplication.java
@@ -0,0 +1,11 @@
+package com.lft.testdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDockerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestDockerApplication.class, args);
+    }
+}
